3b301d06297aeb808bdaab9fab9679dbf18f21a1
HostileCat <hjohanalejandro@gmail.com>
2024-06-07 17:54:44 -0500

    Ajustes al responsive

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const userIcon = document.getElementById("userIcon");
-    const subitem = document.getElementById("subitem");
+  const userIcon = document.getElementById("userIcon");
+  const subitem = document.getElementById("subitem");
 
-    userIcon.addEventListener("click", function(event) {
-        event.preventDefault(); // Previene el comportamiento por defecto del enlace
-        if (subitem.style.display === "none" || subitem.style.display === "") {
-            subitem.style.display = "flex";
-        } else {
-            subitem.style.display = "none";
-        }
-    });
+  userIcon.addEventListener("click", function(event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del enlace
+    if (subitem.style.display === "none" || subitem.style.display === "") {
+      subitem.style.display = "flex";
+    } else {
+      subitem.style.display = "none";
+    }
+  });
 });
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const mobileNav = document.getElementById('mobile-nav');
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const mobileNav = document.getElementById('mobile-nav');
 
-    hamburgerMenu.addEventListener('click', function () {
-        mobileNav.classList.toggle('show');
-    });
+  hamburgerMenu.addEventListener('click', function () {
+      mobileNav.classList.toggle('show');
+  });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const circle = document.getElementById('circle');
-    const hiddenActions = document.getElementById('hidden__actions');
+  const user__icon = document.getElementById('user__icon');
+  const hiddenActions = document.getElementById('hidden__actions');
 
-    circle.addEventListener('click', function () {
-        hiddenActions.classList.toggle('show');
-    });
+  user__icon.addEventListener('click', function () {
+      hiddenActions.classList.toggle('show');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const mainMenu = document.getElementById('main__menu')
+  const arrow = document.getElementById('arrow')
+  const mainSelectCategory = document.getElementById('main__selectCategory');
+  const mainSelectDish = document.getElementById('main__selectDish');
+
+  arrow.addEventListener('click', function () {
+    mainSelectCategory.classList.toggle('show');
+    mainSelectDish.classList.toggle('show');
+    arrow.classList.toggle('show');
+    mainMenu.classList.toggle('show');
+  });
 });
